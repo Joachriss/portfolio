179b068658640348bc79dc6d6c15b427cdb9e92f
Johannes Mwasanyila <joachriss@gmail.com>
2025-04-27 14:41:56 +0300

    Test deploy

---
+++ src/components/WhatIDo.tsx
@@ -0,0 +1,6 @@
+
+export const WhatIDo = () => {
+  return (
+    <div>WhatIDo</div>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
+import { AboutSection } from "../components/AboutSection";
+import { WhatIDo } from "../components/WhatIDo";
+
 export const Home = () => {
     return (
-      <section className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-white px-6 pt-16">
-        <div className="text-center">
-          <h2 className="text-4xl md:text-6xl font-bold text-gray-800 mb-4">
+      <section className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue-50 to-white">
+        <div className="text-center h-screen w-full flex flex-col items-center justify-center bg-[#191919]">
+          <h2 className="text-4xl md:text-6xl font-bold text-gray-200 mb-4">
             Hi, I'm Johannes Mwasanyila
           </h2>
           <p className="text-xl text-gray-600 mb-6">
@@ -15,6 +18,8 @@ export const Home = () => {
             View My Work
           </a>
         </div>
+        <AboutSection />
+        <WhatIDo />
       </section>
     );
   };
